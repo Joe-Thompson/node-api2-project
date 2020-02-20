ed7ec3670855084303f260e0aea68ff7be2691e4
Joe <joe@pop-os.localdomain>
2020-02-19 20:55:51 -0500

    welcome message

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ server.use("/api/posts", postRouter);
 
 const port = process.env.PORT || 4000;
 
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "welcome to our little hobbit hole",
+  })
+});
+
 server.listen(port, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
 });
